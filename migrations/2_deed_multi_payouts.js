ccd07abf8c42ebbbbfc646e1d1f7c1662b12284c
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-20 21:17:45 -0700

    Fix typo in migration

--- migrations/2_deed_multi_payouts.js
+++ migrations/2_deed_multi_payouts.js
@@ -1,4 +1,4 @@
-const DeedMultiPayouts = artifacts.require("Deed");
+const DeedMultiPayouts = artifacts.require("DeedMultiPayouts");
 
 module.exports = function (deployer, _network, accounts) {
   deployer.deploy(DeedMultiPayouts, accounts[0], accounts[1], 5, {value: 100});
