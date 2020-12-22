b935191ca2110ce3be542e2935a30affc076d968
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-21 23:17:58 -0700

    Fix small errors and change time in migration to 1

--- migrations/2_deed_multi_payouts.js
+++ migrations/2_deed_multi_payouts.js
@@ -1,5 +1,5 @@
 const DeedMultiPayouts = artifacts.require("DeedMultiPayouts");
 
 module.exports = function (deployer, _network, accounts) {
-  deployer.deploy(DeedMultiPayouts, accounts[0], accounts[1], 5, {value: 100});
+  deployer.deploy(DeedMultiPayouts, accounts[0], accounts[1], 1, {value: 100});
 };
--- test/deed_multi_payouts.js
+++ test/deed_multi_payouts.js
@@ -8,10 +8,10 @@ contract('DeedMultiPayouts', (accounts) => {
 
   it('Should withdraw for all payouts (1)', async () => {
     for(let i = 0; i < 4; i++) {
-      const balanceBefore = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
       await new Promise(resolve => setTimeout(resolve, 1000));
-      deedMultiPayouts.withdraw({from: accounts[0]});
-      const balanceAfter = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      deedMultiPayouts.withdraw({from: accounts[1]});
+      const balanceAfter = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
       assert(balanceAfter.sub(balanceBefore).toNumber() === 25);
     }
   });
@@ -23,12 +23,12 @@ contract('DeedMultiPayouts', (accounts) => {
       1,
       {value: 100}
     );
-    
+
     for(let i = 0; i < 2; i++) {
-      const balanceBefore = web3.utils.toBN(await web3.getBalance(accounts[1]));
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      deedMultiPayouts.withdraw({from: accounts[0]});
-      const balanceAfter = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      deedMultiPayouts.withdraw({from: accounts[1]});
+      const balanceAfter = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
       assert(balanceAfter.sub(balanceBefore).toNumber() === 50);
     }
   });
