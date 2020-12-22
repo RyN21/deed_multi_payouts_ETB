80f60db771fa5983bc67b7fa4d1561e0f0dff64d
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-21 22:52:03 -0700

    Create test if withdraw is too early

--- test/deed_multi_payouts.js
+++ test/deed_multi_payouts.js
@@ -5,4 +5,19 @@ contract('DeedMultiPayouts', (accounts) => {
   before(async () => {
     deedMultiPayouts = await DeedMultiPayouts.deployed();
   });
+
+  it('Should NOT withdraw if too early', async () => {
+    const deedMultiPayouts = await DeedMultiPayouts.new(
+      accounts[0],
+      accounts[1],
+      5,
+      {value: 100}
+    );
+    try {
+      await deedMultiPayouts.withdraw({from: accounts[0]});
+    } catch(e) {
+      assert(e.message.includes('too early'));
+    }
+    assert(false);
+  });
 })
