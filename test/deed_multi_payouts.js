b409928b5bc5a14d19dfa55d1f8685082b13e4e7
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-21 22:54:29 -0700

    Create test withdraw should work only for beneficiary

--- test/deed_multi_payouts.js
+++ test/deed_multi_payouts.js
@@ -14,9 +14,24 @@ contract('DeedMultiPayouts', (accounts) => {
       {value: 100}
     );
     try {
+      await deedMultiPayouts.withdraw({from: accounts[1]});
+    } catch(e) {
+      assert(e.message.includes('Too early.'));
+    }
+    assert(false);
+  });
+
+  it('Should NOT withdraw if caller is not beneficiary', async () => {
+    const deedMultiPayouts = await DeedMultiPayouts.new(
+      accounts[0],
+      accounts[1],
+      5,
+      {value: 100}
+    );
+    try {
       await deedMultiPayouts.withdraw({from: accounts[0]});
     } catch(e) {
-      assert(e.message.includes('too early'));
+      assert(e.message.includes('Beneficiary only.'));
     }
     assert(false);
   });
