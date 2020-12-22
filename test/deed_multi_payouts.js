e37f9c2aba194ead4411b7cf527a56709a88b387
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-21 23:35:00 -0700

    Add test to check for error when no payouts are left

--- test/deed_multi_payouts.js
+++ test/deed_multi_payouts.js
@@ -8,11 +8,11 @@ contract('DeedMultiPayouts', (accounts) => {
 
   it('Should withdraw for all payouts (1)', async () => {
     for(let i = 0; i < 4; i++) {
-      const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+      const initialBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
       await new Promise(resolve => setTimeout(resolve, 1000));
       await deedMultiPayouts.withdraw({from: accounts[1]});
-      const balanceAfter = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
-      assert(balanceAfter.sub(balanceBefore).toNumber() === 25);
+      const finalBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+      assert(finalBalance.sub(initialBalance).toNumber() === 25);
     }
   });
 
@@ -23,13 +23,12 @@ contract('DeedMultiPayouts', (accounts) => {
       1,
       {value: 100}
     );
-
     for(let i = 0; i < 2; i++) {
-      const balanceBefore = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+      const initialBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
       await new Promise(resolve => setTimeout(resolve, 2000));
       await deedMultiPayouts.withdraw({from: accounts[1]});
-      const balanceAfter = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
-      assert(balanceAfter.sub(balanceBefore).toNumber() === 50);
+      const finalBalance = web3.utils.toBN(await web3.eth.getBalance(accounts[1]));
+      assert(finalBalance.sub(initialBalance).toNumber() === 50);
     }
   });
 
@@ -37,7 +36,7 @@ contract('DeedMultiPayouts', (accounts) => {
     const deedMultiPayouts = await DeedMultiPayouts.new(
       accounts[0],
       accounts[1],
-      5,
+      1,
       {value: 100}
     );
     try {
@@ -49,11 +48,20 @@ contract('DeedMultiPayouts', (accounts) => {
     assert(false);
   });
 
+  it('Should NOT withdraw if no payout is left', async () => {
+    try {
+      await deedMultiPayouts.withdraw({from: accounts[1]});
+    } catch (e) {
+      assert(e.message.includes('No payout left.'))
+      return;
+    }
+  })
+
   it('Should NOT withdraw if caller is not beneficiary', async () => {
     const deedMultiPayouts = await DeedMultiPayouts.new(
       accounts[0],
       accounts[1],
-      5,
+      1,
       {value: 100}
     );
     try {
