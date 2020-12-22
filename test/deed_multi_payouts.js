7ddf20bf974673f2036f2a39d73ae3717c93cc45
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-12-21 23:11:38 -0700

    Add withdraw tests

--- test/deed_multi_payouts.js
+++ test/deed_multi_payouts.js
@@ -6,6 +6,33 @@ contract('DeedMultiPayouts', (accounts) => {
     deedMultiPayouts = await DeedMultiPayouts.deployed();
   });
 
+  it('Should withdraw for all payouts (1)', async () => {
+    for(let i = 0; i < 4; i++) {
+      const balanceBefore = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      deedMultiPayouts.withdraw({from: accounts[0]});
+      const balanceAfter = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      assert(balanceAfter.sub(balanceBefore).toNumber() === 25);
+    }
+  });
+
+  it('Should withdraw for all payouts (2)', async () => {
+    const deedMultiPayouts = await DeedMultiPayouts.new(
+      accounts[0],
+      accounts[1],
+      1,
+      {value: 100}
+    );
+    
+    for(let i = 0; i < 2; i++) {
+      const balanceBefore = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      deedMultiPayouts.withdraw({from: accounts[0]});
+      const balanceAfter = web3.utils.toBN(await web3.getBalance(accounts[1]));
+      assert(balanceAfter.sub(balanceBefore).toNumber() === 50);
+    }
+  });
+
   it('Should NOT withdraw if too early', async () => {
     const deedMultiPayouts = await DeedMultiPayouts.new(
       accounts[0],
